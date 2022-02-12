bdd0fb12bf6cce980e01e6cfcd0090a3f41bfb76
Joao Roberto <joaocte@gmail.com>
2022-02-12 06:39:34 -0300

    melhorias no código

--- ReplaceRegex/Program.cs
+++ ReplaceRegex/Program.cs
@@ -14,37 +14,40 @@ namespace ReplaceRegex
 {
     class Program
     {
+        private const string StringRegex = "request\\.getParameter\\(\"*.*\"*|param\\[.*\\]\\)\\;";
+
         static void Main(string[] args)
         {
+            ReplaceConfig replaceConfig = ObterConfiguracao();
 
-            var builder = new ConfigurationBuilder()
-                 .SetBasePath(Directory.GetCurrentDirectory())
-                 .AddJsonFile("appsettings.json", optional: false);
-            IConfiguration config = builder.Build();
-
-            ReplaceConfig replaceConfig = config.GetSection("ReplaceConfig").Get<ReplaceConfig>();
-            var stringRegex = "^request\\.getParameter\\(\"*.*\"*|param\\[.*\\]\\)\\;";
-            var regex = new Regex(stringRegex, RegexOptions.IgnorePatternWhitespace);
-            string line = null;
+            var regex = new Regex(StringRegex, RegexOptions.IgnorePatternWhitespace);
             var logs = new List<ArquivoCsv>();
+            var arquivosParaProcessar = Directory.GetFiles(replaceConfig.Folder, $"*{replaceConfig.FileType}", SearchOption.AllDirectories);
             bool arquivoModificado = false;
-            foreach (string file in Directory.GetFiles(replaceConfig.Folder, $"*{replaceConfig.FileType}", SearchOption.AllDirectories))
+
+            foreach (string file in arquivosParaProcessar)
             {
                 long line_number = 1;
 
                 var linhas = File.ReadAllLines(file);
+                var existeAlgumaLinhaParaAlterar = linhas.Any(x => regex.IsMatch(x) && !x.Contains("ESAPI.encoder().encodeForHTML("));
 
-                if (linhas.Any(x => regex.IsMatch(x) && !x.Contains("ESAPI.encoder().encodeForHTML(")))
+                if (existeAlgumaLinhaParaAlterar)
                 {
                     linhas = null;
                     using (StreamReader reader = new StreamReader(file))
                     using (StreamWriter writer = new StreamWriter($"{file}{replaceConfig.FileTypeTemp}"))
                     {
+                        string line = null;
+
                         while ((line = reader.ReadLine()) != null)
                         {
                             if (regex.IsMatch(line) && !line.Contains("ESAPI.encoder().encodeForHTML("))
                             {
-                                var newContent = Regex.Replace(line, "request\\.getParameter\\(\" *.* \"*|param\\[.*\\]\\)\\;", $"ESAPI.encoder().encodeForHTML({line.Replace(");", ")")});", RegexOptions.IgnorePatternWhitespace);
+                                var match = Regex.Match(line, StringRegex).Value.Replace(";", "");
+
+
+                                var newContent = Regex.Replace(line, StringRegex, $"ESAPI.encoder().encodeForHTML({match});", RegexOptions.IgnorePatternWhitespace);
                                 writer.WriteLine(newContent);
                                 logs.Add(RetornaLinhaFormatada(file, line_number, line, newContent));
                                 arquivoModificado = true;
@@ -61,7 +64,7 @@ namespace ReplaceRegex
                         var srReader = new StreamReader($"{file}{replaceConfig.FileTypeTemp}");
                         var strFileContents = srReader.ReadToEnd();
                         srReader.Close();
-                        strFileContents = "<%@ page import=\"org.owasp.esapi.*\"%>" + Environment.NewLine + strFileContents;
+                        strFileContents = $"<%@ page import=\"org.owasp.esapi.*\"%>{Environment.NewLine}{strFileContents}";
                         var swWriter = new StreamWriter($"{file}{replaceConfig.FileTypeTemp}", false);
                         swWriter.Write(strFileContents);
                         swWriter.Flush();
@@ -72,7 +75,15 @@ namespace ReplaceRegex
             File.AppendAllText($"{Directory.GetCurrentDirectory()}/log.json", JsonConvert.SerializeObject(logs));
         }
 
-
+        private static ReplaceConfig ObterConfiguracao()
+        {
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json", optional: false);
+            IConfiguration config = builder.Build();
+            ReplaceConfig replaceConfig = config.GetSection("ReplaceConfig").Get<ReplaceConfig>();
+            return replaceConfig;
+        }
 
         private static ArquivoCsv RetornaLinhaFormatada(string file, long line_number, string line, string newContent)
         {
